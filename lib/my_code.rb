0117a9221ba7823aed5f6f60af012818be8851c6
Xavier Carty <xaviercarty@gmail.com>
2020-02-24 16:55:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -55,9 +55,9 @@ def reduce_to_total(source_array,starting_point)
   total = 0
   while i < source_array.length
     total += source_array[i]
-    new_array << total
     i += 1
   end
+  new_array << total
   new_array
 
 end
