d0861dc031d3e3fd86aba5721d46d81bc008678e
Xavier Carty <xaviercarty@gmail.com>
2020-02-24 17:02:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -50,15 +50,15 @@ new_array
 end
 [1,2,3]
 
-def reduce_to_total(source_array,starting_point = 0 )
-  new_array = []
+def reduce_to_total(source_array,starting_point=0 )
+  news = starting_point
   i = 0
-  total = 0
+
   while i < source_array.length
-    total += source_array[i]
+    news += source_array[i]
     i += 1
   end
-  total
+  
 end
 
 def reduce_to_total(source_array,starting_point)
@@ -71,4 +71,23 @@ def reduce_to_total(source_array,starting_point)
   end
   starting_point
 
+end
+
+
+def reduce_to_all_true(source_array)
+  i = 0
+  while i < source_array.length do
+    return false if !source_array[i]
+    i += 1
+  end
+  return true
+end
+
+def reduce_to_any_true(source_array)
+  i = 0
+  while i < source_array.length do
+    return true if source_array[i]
+    i += 1
+  end
+  return false
 end
