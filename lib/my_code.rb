a5510cf2b38b8f8bc498bb23c8366791cac0a40b
Xavier Carty <xaviercarty@gmail.com>
2020-02-24 16:35:36 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,13 +3,12 @@
 def map_to_negativize(array)
 
   new_array = []
-  i = 0 
+  i = 0
 
-  while i < array.length 
-     total = i * -1 
-     new_array << total 
-     i += 1 
-  end 
+  while i < array.length
+     total = i * -1
+     new_array << total
+     i += 1
+  end
 
 end 
-      
