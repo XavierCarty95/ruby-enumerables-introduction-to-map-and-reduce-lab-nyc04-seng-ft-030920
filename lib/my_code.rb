b1651c120d1d9bd9d472ebd8cbf3df18553574d1
Xavier Carty <xaviercarty@gmail.com>
2020-02-24 16:35:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,15 @@
 # My Code here....
+
+def map_to_negativize(array)
+
+  new_array = []
+  i = 0 
+
+  while i < array.length 
+     total = i * -1 
+     new_array << total 
+     i += 1 
+  end 
+
+end 
+      
